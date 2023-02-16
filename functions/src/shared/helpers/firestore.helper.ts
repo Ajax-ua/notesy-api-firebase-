3c4310b76e7252246847560f741ef8ba19d5d5aa
Anton Myerkin <anton@2muchcoffee.com>
2023-02-16 13:25:11 +0200

    feat(shared:helpers:firestore): transform firebase timestamp to js timestamp

--- functions/src/shared/helpers/firestore.helper.ts
+++ functions/src/shared/helpers/firestore.helper.ts
@@ -1,8 +1,20 @@
-import { DocumentSnapshot } from 'firebase-admin/firestore';
+import { DocumentSnapshot, Timestamp } from 'firebase-admin/firestore';
 
 export function convertDocSnapshotTo<T>(snapshot: DocumentSnapshot): T {
+  const data = snapshot.data();
+  if (!data) {
+    return undefined as T;
+  }
+
+  if (data.createdAt instanceof Timestamp) {
+    data.createdAt = data.createdAt.toMillis();
+  }
+  if (data.updatedAt instanceof Timestamp) {
+    data.updatedAt = data.updatedAt.toMillis();
+  }
+
   return {
     id: snapshot.id,
-    ...snapshot.data(),
+    ...data,
   } as T;
 }
