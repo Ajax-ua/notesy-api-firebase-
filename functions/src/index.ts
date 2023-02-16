fb68d456e5f06cfaf30824ded70e35087f032659
Anton Myerkin <anton@2muchcoffee.com>
2023-02-16 11:00:01 +0200

    feat(index.ts): apply express app and triggers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+import * as app from './app';
+import * as triggers from './triggers';
+
+admin.initializeApp();
+
+exports.triggers = triggers;
+
+exports.v1 = functions.https.onRequest(app);
