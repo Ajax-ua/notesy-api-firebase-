ae126e182723128926e9f216a35f3687e09b5226
Anton Myerkin <anton@2muchcoffee.com>
2023-02-16 13:14:06 +0200

    feat(api:notes:service): wait for timestamp fields are created before returning created note

--- functions/src/api/notes/notes.service.ts
+++ functions/src/api/notes/notes.service.ts
@@ -1,30 +1,38 @@
 import * as admin from 'firebase-admin';
+import { DocumentSnapshot } from 'firebase-admin/firestore';
 
 import { Note } from '../../shared/models/note.model';
+import { convertDocSnapshotTo } from '../../shared/helpers/firestore.helper';
 
-function convertDocSnapshotToNote(snapshot): Note {
-  return {
-    ...snapshot.data(),
-    id: snapshot.id,
-  } as Note;
-}
 
 export async function createNote(note: Omit<Note, 'id'>): Promise<Note> {
-  const noteItem = await (await admin.firestore().collection('notes').add(note)).get();
-  return {
-    id: noteItem.id,
-    ...noteItem.data(),
-  } as Note;
+  const noteRef = await (await admin.firestore().collection('notes').add(note));
+
+  // wait until timestamp fields are added by trigger
+  const noteSnapshot: DocumentSnapshot = await new Promise((resolve, reject) => {
+    noteRef.onSnapshot(
+      snapshot => {
+        if (snapshot.data()?.createdAt) {
+          resolve(snapshot);
+        }
+      },
+      err => {
+        reject(err);
+      }
+    );
+  });
+
+  return convertDocSnapshotTo<Note>(noteSnapshot);
 }
 
 export async function getNotes(): Promise<Note[]> {
   const notesCollection = await admin.firestore().collection('notes').get();
-  return notesCollection.docs.map(doc => convertDocSnapshotToNote(doc));
+  return notesCollection.docs.map(doc => convertDocSnapshotTo<Note>(doc));
 }
 
 export async function getNote(noteId: string): Promise<Note> {
   const noteItem = await admin.firestore().collection('notes').doc(noteId).get();
-  return convertDocSnapshotToNote(noteItem);
+  return convertDocSnapshotTo<Note>(noteItem);
 }
 
 export async function patchNote({ id, ...note }: Note): Promise<Note> {
@@ -32,12 +40,12 @@ export async function patchNote({ id, ...note }: Note): Promise<Note> {
 
   await doc.set(note, { merge: true });
 
-  return convertDocSnapshotToNote(await doc.get());
+  return convertDocSnapshotTo<Note>(await doc.get());
 }
 
 export async function deleteNote(noteId: string): Promise<Note> {
   const doc = admin.firestore().collection('notes').doc(noteId);
   const noteSnapshot = await doc.get();
   await doc.delete();
-  return convertDocSnapshotToNote(noteSnapshot);
+  return convertDocSnapshotTo<Note>(noteSnapshot);
 }
